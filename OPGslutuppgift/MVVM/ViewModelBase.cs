93ffa29b23b8a4856df13300ca59c5e9bace2eeb
AngelosLekkas <lekkas.angelos@gmail.com>
2025-10-21 16:19:01 +0200

    Skapat alla windows & ViewModels för dom samt klasser utifrån klassdiagramet.

---
+++ OPGslutuppgift/MVVM/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPGslutuppgift.MVVM
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        //Parent klass som implementerar INotifyPropertyChanged för alla viewmodels.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
